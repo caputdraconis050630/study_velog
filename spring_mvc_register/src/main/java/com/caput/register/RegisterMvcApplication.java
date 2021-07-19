67283af2a12edf7fa20dd58857a39d685e77dc80
caputdraconis050630 <caputdraconis@kakao.com>
2021-07-20 06:12:23 +0900

    making spring project with MVC

---
+++ spring_mvc_register/src/main/java/com/caput/register/RegisterMvcApplication.java
@@ -0,0 +1,13 @@
+package com.caput.register;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RegisterMvcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RegisterMvcApplication.class, args);
+    }
+
+}
